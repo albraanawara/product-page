3b397a50963a38151696c29d1f6e6801b02ec838
albraa nawara <albraa nawara@gmail.com>
2026-02-20 23:47:19 +0200

    refactor: clean up template comments in ProductView.vue

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -4,27 +4,27 @@ import { useRoute } from 'vue-router'
 import ProductDetails from '../components/ProductDetails.vue'
 import ProductCard from '../components/ProductCard.vue'
 
-// Props from App.vue
+
 const props = defineProps(['products'])
 
-// Emits up to App.vue
+
 const emit = defineEmits(['buy'])
 
-// Read ID from route
+
 const route = useRoute()
 const productId = Number(route.params.id)
 
-// Current product
+
 const product = computed(() =>
   props.products.find(p => p.id === productId)
 )
 
-// Related products = all except current
+
 const relatedProducts = computed(() =>
   props.products.filter(p => p.id !== productId)
 )
 
-// Lifecycle hooks
+
 onMounted(() => console.log(`ProductView mounted for ID: ${productId}`))
 onUnmounted(() => console.log('ProductView unmounted'))
 
@@ -32,13 +32,13 @@ onUnmounted(() => console.log('ProductView unmounted'))
 
 <template>
   <div v-if="product">
-    <!-- Product details -->
+    
     <ProductDetails
       :product="product"
       @buy="emit('buy', $event)"
     />
 
-    <!-- Related products -->
+    
     <h2 class="text-2xl font-bold mt-8 px-6">Related Products</h2>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
       <ProductCard
